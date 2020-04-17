84e012b4421e6557dd4f0afb97754e51bbc59813
Serrhii07 <serge070989@gmail.com>
2020-04-17 12:34:06 +0300

    add color switch task

---
+++ js/index.js
@@ -0,0 +1,42 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const bodyRef = document.querySelector('body');
+const btnStartRef = document.querySelector('[data-action="start"]');
+const btnStopRef = document.querySelector('[data-action="stop"]');
+
+btnStartRef.addEventListener('click', handleBodyColorStart);
+btnStopRef.addEventListener('click', handleBodyColorStop);
+
+let intervalId = null;
+let isActive = false;
+
+function handleBodyColorStart() {
+  if (isActive) {
+    return;
+  }
+
+  intervalId = setInterval(() => {
+    isActive = true;
+    changeColor();
+  }, 1000);
+}
+
+function changeColor() {
+  bodyRef.style.backgroundColor = colors[randomIntegerFromInterval(0, 5)];
+}
+
+function handleBodyColorStop() {
+  isActive = false;
+  clearInterval(intervalId);
+}
